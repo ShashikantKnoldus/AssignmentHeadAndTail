490ddb6f38558f8d4b4e894b4e15b444681eb2a9
ShashikantKnoldus <shashikant.tyagi@knoldus.com>
2021-11-01 21:51:14 +0530

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "AssignmentHeadAndTail"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/test/scala/edu/knoldus/Lists.scala
@@ -0,0 +1,15 @@
+package edu.knoldus
+
+class Lists {
+  def isort(xs: List[Int]): List[Int] = {
+
+    if (xs.isEmpty) Nil
+    else insert(xs.head, isort(xs.tail))
+
+  }
+
+  def insert(x: Int, xs: List[Int]): List[Int] =
+    if (xs.isEmpty || x <= xs.head) x :: xs
+   else xs.head :: insert(x, xs.tail)
+
+}
